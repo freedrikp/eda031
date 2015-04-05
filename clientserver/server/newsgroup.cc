cc65d9330c405014d8f3524436d7efb8750ca7b8
Christian <christian@ubuntu.(none)>
2015-04-05 19:53:10 +0200

    improved newsgroups

--- clientserver/server/newsgroup.cc
+++ clientserver/server/newsgroup.cc
@@ -1,6 +1,9 @@
 #include "newsgroup.h"
+#include "article.h"
+#include <vector>
 
-Newsgroup::Newsgroup(std::string name, size_t id): name(name), id(id){}
+
+Newsgroup::Newsgroup(std::string name, size_t id): name(name), id(id), article_counter(0){}
 
 size_t Newsgroup::getID() const{
   return id;
@@ -8,4 +11,21 @@ size_t Newsgroup::getID() const{
 
 std::string Newsgroup::getName() const{
   return name;
+}
+
+Newsgroup::~news_group(){
+}
+
+void Newsgroup::new_article(std::string& title, std::string& author, std::string& content){
+      article art(article_ids, title, author,content);
+      article_counter++;
+      add_article(art);
+}
+
+void Newsgroup::add_article(article a){
+      articles.push_back(a);
+}
+
+bool Newsgroup::remove_article(int id){
+
 }
