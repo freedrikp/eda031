3c3e6a98afc15e8f15fd66d6338cd1674b5de519
freedrikp <fredrik40@msn.com>
2015-04-07 08:36:40 +0200

    Kommenterat ut lite med memorydatabase, var lite missförstånd.

--- clientserver/server/newsgroup.cc
+++ clientserver/server/newsgroup.cc
@@ -3,7 +3,7 @@
 #include <vector>
 
 
-Newsgroup::Newsgroup(std::string name, size_t id): name(name), id(id), article_counter(0){}
+Newsgroup::Newsgroup(std::string name, size_t id): name(name), id(id){} //, article_counter(0){}
 
 size_t Newsgroup::getID() const{
   return id;
@@ -13,19 +13,18 @@ std::string Newsgroup::getName() const{
   return name;
 }
 
-Newsgroup::~news_group(){
-}
 
-void Newsgroup::new_article(std::string& title, std::string& author, std::string& content){
-      article art(article_ids, title, author,content);
-      article_counter++;
-      add_article(art);
-}
 
-void Newsgroup::add_article(article a){
-      articles.push_back(a);
-}
-
-bool Newsgroup::remove_article(int id){
-
-}
+// void Newsgroup::new_article(std::string& title, std::string& author, std::string& content){
+//       article art(article_ids, title, author,content);
+//       article_counter++;
+//       add_article(art);
+// }
+//
+// void Newsgroup::add_article(article a){
+//       articles.push_back(a);
+// }
+//
+// bool Newsgroup::remove_article(int id){
+//
+// }
