45f2524da66787a90ca1c5de5eb439f38da303b4
dat11fak <dat11fak@student.lth.se>
2015-04-09 14:06:33 +0200

    Moved communications from ramserver to servermessagehandler. ramserver is still ugly.

--- clientserver/server/ramserver.cc
+++ clientserver/server/ramserver.cc
@@ -19,86 +19,6 @@
 
 using namespace std;
 
-/*
-* Read an integer from a client.
-*/
-unsigned char readCode(const shared_ptr<Connection>& conn) {
-	return conn->read();
-}
-
-
-int readBytes(const shared_ptr<Connection>& conn) {
-	unsigned char byte1 = conn->read();
-	unsigned char byte2 = conn->read();
-	unsigned char byte3 = conn->read();
-	unsigned char byte4 = conn->read();
-	return (byte1 << 24) | (byte2 << 16) | (byte3 << 8) | byte4;
-}
-
-/*
-* Read an integer from a client.
-*/
-int readInt(const shared_ptr<Connection>& conn) {
-	if(readCode(conn) != Protocol::PAR_NUM){
-		throw ProtocolViolationException("Read Int");
-	}
-	return readBytes(conn);
-}
-
-/*
-* Read an integer from a client.
-*/
-string readString(const shared_ptr<Connection>& conn) {
-	if(readCode(conn) != Protocol::PAR_STRING){
-		throw ProtocolViolationException("Read String");
-	}
-	string s;
-	int n = readBytes(conn);
-
-	for(int i = 0; i < n; ++i){
-		s += readCode(conn);
-	}
-	return s;
-}
-
-/*
-* Send a string to a client.
-*/
-void writeString(const shared_ptr<Connection>& conn, const string& s) {
-	conn->write(Protocol::PAR_STRING);
-	size_t value = s.size();
-
-	conn->write((value >> 24) & 0xFF);
-	conn->write((value >> 16) & 0xFF);
-	conn->write((value >> 8) & 0xFF);
-	conn->write(value & 0xFF);
-
-	for (char c : s) {
-		conn->write(c);
-	}
-
-}
-
-/*
-* Send an code to the server as one byte.
-*/
-void writeCode(const shared_ptr<Connection>& conn, unsigned char value) {
-	cout << "writeCode with parameter: " << (value&0xFF) << endl;
-	conn->write(value);
-}
-
-/*
-* Send an integer to the server as four bytes.
-*/
-void writeNumber(const shared_ptr<Connection>& conn, int value) {
-	conn->write(Protocol::PAR_NUM);
-	conn->write((value >> 24) & 0xFF);
-	conn->write((value >> 16) & 0xFF);
-	conn->write((value >> 8) & 0xFF);
-	conn->write(value & 0xFF);
-}
-
-
 int main(int argc, char* argv[]){
 	cout << "Starting server... " << endl;
 	if (argc != 2) {
@@ -127,148 +47,148 @@ int main(int argc, char* argv[]){
 		auto conn = server.waitForActivity();
 		if (conn != nullptr) {
 			try {
-				int kase = readCode(conn);
+				int kase = ServerMessageHandler::readCode(conn);
 				cout << "kase: " << kase << endl;
 				switch(kase){
 					case Protocol::COM_LIST_NG:{
 						cout << "@COM_LIST_NG" << endl;
-						if(readCode(conn) != Protocol::COM_END){
+						if(ServerMessageHandler::readCode(conn) != Protocol::COM_END){
 							throw ProtocolViolationException("Create ng");
 						}
-						writeCode(conn, Protocol::ANS_LIST_NG);
+						ServerMessageHandler::writeCode(conn, Protocol::ANS_LIST_NG);
 						vector<Newsgroup> groups = database.getNewsgroups();
 						cout << "writing number of groups:" << groups.size() <<endl;
-						writeNumber(conn, groups.size());
+						ServerMessageHandler::writeNumber(conn, groups.size());
 						for(auto it = groups.begin(); it < groups.end(); ++it){
 							cout << "writing nummber: "<< it->getID() << " and name " <<it->getName()<<endl;
-							writeNumber(conn, it->getID());
-							writeString(conn, it->getName());
+							ServerMessageHandler::writeNumber(conn, it->getID());
+							ServerMessageHandler::writeString(conn, it->getName());
 						}
-						writeCode(conn, Protocol::ANS_END);
+						ServerMessageHandler::writeCode(conn, Protocol::ANS_END);
 						break;
 					}
 					case Protocol::COM_CREATE_NG:{
 						cout << "@COM_CREATE_NG" << endl;
-						string name = readString(conn);
-						if(readCode(conn) != Protocol::COM_END){
+						string name = ServerMessageHandler::readString(conn);
+						if(ServerMessageHandler::readCode(conn) != Protocol::COM_END){
 								throw ProtocolViolationException("List ng");
 						}
-						writeCode(conn, Protocol::ANS_CREATE_NG);
+						ServerMessageHandler::writeCode(conn, Protocol::ANS_CREATE_NG);
 						if(database.addNewsgroup(name)){
-							writeCode(conn, Protocol::ANS_ACK);
+							ServerMessageHandler::writeCode(conn, Protocol::ANS_ACK);
 						} else {
-							writeCode(conn, Protocol::ANS_NAK);
-							writeCode(conn, Protocol::ERR_NG_ALREADY_EXISTS);
+							ServerMessageHandler::writeCode(conn, Protocol::ANS_NAK);
+							ServerMessageHandler::writeCode(conn, Protocol::ERR_NG_ALREADY_EXISTS);
 						}
-						writeCode(conn, Protocol::ANS_END);
+						ServerMessageHandler::writeCode(conn, Protocol::ANS_END);
 						
 						break;
 					}
 					case Protocol::COM_DELETE_NG:{
 						cout << "@COM_DELETE_NG" << endl;
-						size_t nGroupID = static_cast<size_t>(readInt(conn));
-						if(readCode(conn) != Protocol::COM_END){
+						size_t nGroupID = static_cast<size_t>(ServerMessageHandler::readInt(conn));
+						if(ServerMessageHandler::readCode(conn) != Protocol::COM_END){
 							throw ProtocolViolationException("Delete ng");
 						}
-						writeCode(conn, Protocol::ANS_DELETE_NG);
+						ServerMessageHandler::writeCode(conn, Protocol::ANS_DELETE_NG);
 						if(database.removeNewsgroup(nGroupID)){
-							writeCode(conn, Protocol::ANS_ACK);
+							ServerMessageHandler::writeCode(conn, Protocol::ANS_ACK);
 						} else {
-							writeCode(conn, Protocol::ANS_NAK);
-							writeCode(conn, Protocol::ERR_NG_DOES_NOT_EXIST);
+							ServerMessageHandler::writeCode(conn, Protocol::ANS_NAK);
+							ServerMessageHandler::writeCode(conn, Protocol::ERR_NG_DOES_NOT_EXIST);
 						}
-						writeCode(conn, Protocol::ANS_END);
+						ServerMessageHandler::writeCode(conn, Protocol::ANS_END);
 						break;
 					}
 					case Protocol::COM_LIST_ART:{
 						cout << "@COM_LIST_ART" << endl;
-						size_t nGroupID = static_cast<size_t>(readInt(conn));
-						if(readCode(conn) != Protocol::COM_END){
+						size_t nGroupID = static_cast<size_t>(ServerMessageHandler::readInt(conn));
+						if(ServerMessageHandler::readCode(conn) != Protocol::COM_END){
 							throw ProtocolViolationException("List art");
 						}
-						writeCode(conn, Protocol::ANS_LIST_ART);
+						ServerMessageHandler::writeCode(conn, Protocol::ANS_LIST_ART);
 						try{
 							cout << "# apa 1" << endl;
 							vector<Article> articles = database.getArticles(nGroupID);
 							cout << "# apa 2" << endl;
-							writeCode(conn, Protocol::ANS_ACK);
-							writeNumber(conn, articles.size());
+							ServerMessageHandler::writeCode(conn, Protocol::ANS_ACK);
+							ServerMessageHandler::writeNumber(conn, articles.size());
 							for(auto it = articles.begin(); it < articles.end(); ++it){
 								cout << "writing number: "<< it->getID() << " and name " <<it->getTitle()<<endl;
-								writeNumber(conn, it->getID());
-								writeString(conn, it->getTitle());
+								ServerMessageHandler::writeNumber(conn, it->getID());
+								ServerMessageHandler::writeString(conn, it->getTitle());
 							}
 
 						} catch (NoNewsgroupException nne){
 							cout << "# apa 9" << endl;
-							writeCode(conn, Protocol::ANS_NAK);
-							writeCode(conn, Protocol::ERR_NG_DOES_NOT_EXIST);
+							ServerMessageHandler::writeCode(conn, Protocol::ANS_NAK);
+							ServerMessageHandler::writeCode(conn, Protocol::ERR_NG_DOES_NOT_EXIST);
 						}
-						writeCode(conn, Protocol::ANS_END);
+						ServerMessageHandler::writeCode(conn, Protocol::ANS_END);
 						break;
 					}
 					case Protocol::COM_CREATE_ART:{
-						int newsgroupId = readInt(conn);
-						string author = readString(conn);
-						string title = readString(conn);
-						string text = readString(conn);
-						if(readCode(conn) != Protocol::COM_END){
+						int newsgroupId = ServerMessageHandler::readInt(conn);
+						string author = ServerMessageHandler::readString(conn);
+						string title = ServerMessageHandler::readString(conn);
+						string text = ServerMessageHandler::readString(conn);
+						if(ServerMessageHandler::readCode(conn) != Protocol::COM_END){
 							throw ProtocolViolationException("Create art");
 						}
-						writeCode(conn, Protocol::ANS_CREATE_ART);
+						ServerMessageHandler::writeCode(conn, Protocol::ANS_CREATE_ART);
 						if(database.addArticle(newsgroupId, author, title, text)){
-							writeCode(conn, Protocol::ANS_ACK);
+							ServerMessageHandler::writeCode(conn, Protocol::ANS_ACK);
 						}else {
 
-							writeCode(conn, Protocol::ANS_NAK);
-							writeCode(conn,Protocol::ERR_NG_DOES_NOT_EXIST);
+							ServerMessageHandler::writeCode(conn, Protocol::ANS_NAK);
+							ServerMessageHandler::writeCode(conn,Protocol::ERR_NG_DOES_NOT_EXIST);
 						}
-						writeCode(conn, Protocol::ANS_END);
+						ServerMessageHandler::writeCode(conn, Protocol::ANS_END);
 						break;
 					}
 
 					case Protocol::COM_DELETE_ART:{
-						int newsgroupId = readInt(conn);
-						int articleId = readInt(conn);
-						if(readCode(conn) != Protocol::COM_END){
+						int newsgroupId = ServerMessageHandler::readInt(conn);
+						int articleId = ServerMessageHandler::readInt(conn);
+						if(ServerMessageHandler::readCode(conn) != Protocol::COM_END){
 							throw ProtocolViolationException("Delete art");
 						}
-						writeCode(conn, Protocol::ANS_DELETE_ART);
+						ServerMessageHandler::writeCode(conn, Protocol::ANS_DELETE_ART);
 						try{
 							database.removeArticle(newsgroupId, articleId);
-							writeCode(conn, Protocol::ANS_ACK);
+							ServerMessageHandler::writeCode(conn, Protocol::ANS_ACK);
 						}catch(NoNewsgroupException e){
-							writeCode(conn, Protocol::ANS_NAK);
-							writeCode(conn,Protocol::ERR_NG_DOES_NOT_EXIST);
+							ServerMessageHandler::writeCode(conn, Protocol::ANS_NAK);
+							ServerMessageHandler::writeCode(conn,Protocol::ERR_NG_DOES_NOT_EXIST);
 						}catch(NoArticleException e){
-							writeCode(conn, Protocol::ANS_NAK);
-							writeCode(conn,Protocol::ERR_ART_DOES_NOT_EXIST);
+							ServerMessageHandler::writeCode(conn, Protocol::ANS_NAK);
+							ServerMessageHandler::writeCode(conn,Protocol::ERR_ART_DOES_NOT_EXIST);
 						}
-						writeCode(conn, Protocol::ANS_END);
+						ServerMessageHandler::writeCode(conn, Protocol::ANS_END);
 						break;
 					}
 
 					case Protocol::COM_GET_ART:{
-						int newsgroupId = readInt(conn);
-						int articleId = readInt(conn);
-						if(readCode(conn) != Protocol::COM_END){
+						int newsgroupId = ServerMessageHandler::readInt(conn);
+						int articleId = ServerMessageHandler::readInt(conn);
+						if(ServerMessageHandler::readCode(conn) != Protocol::COM_END){
 							throw ProtocolViolationException("Get art");
 						}
-						writeCode(conn, Protocol::ANS_GET_ART);
+						ServerMessageHandler::writeCode(conn, Protocol::ANS_GET_ART);
 						try{
 							Article article = database.getArticle(newsgroupId, articleId);
-							writeCode(conn, Protocol::ANS_ACK);
-							writeString(conn, article.getTitle());
-							writeString(conn, article.getAuthor());
-							writeString(conn, article.getText());
+							ServerMessageHandler::writeCode(conn, Protocol::ANS_ACK);
+							ServerMessageHandler::writeString(conn, article.getTitle());
+							ServerMessageHandler::writeString(conn, article.getAuthor());
+							ServerMessageHandler::writeString(conn, article.getText());
 						}catch(NoNewsgroupException e){
-							writeCode(conn, Protocol::ANS_NAK);
-							writeCode(conn,Protocol::ERR_NG_DOES_NOT_EXIST);
+							ServerMessageHandler::writeCode(conn, Protocol::ANS_NAK);
+							ServerMessageHandler::writeCode(conn,Protocol::ERR_NG_DOES_NOT_EXIST);
 						}catch(NoArticleException e){
-							writeCode(conn, Protocol::ANS_NAK);
-							writeCode(conn,Protocol::ERR_ART_DOES_NOT_EXIST);
+							ServerMessageHandler::writeCode(conn, Protocol::ANS_NAK);
+							ServerMessageHandler::writeCode(conn,Protocol::ERR_ART_DOES_NOT_EXIST);
 						}
-						writeCode(conn, Protocol::ANS_END);
+						ServerMessageHandler::writeCode(conn, Protocol::ANS_END);
 						break;
 					}
 
--- clientserver/server/servermessagehandler.h
+++ clientserver/server/servermessagehandler.h
@@ -1,16 +1,84 @@
 #include <string>
 #include "connection.h"
 
+using namespace std;
+
 class ServerMessageHandler{
 public:
-  //WIP
+	/*
+	* Read an integer from a client.
+	*/
+	static unsigned char readCode(const shared_ptr<Connection>& conn) {
+		return conn->read();
+	}
+
+	/*
+	* Read an integer from a client.
+	*/
+	static int readInt(const shared_ptr<Connection>& conn) {
+		if(readCode(conn) != Protocol::PAR_NUM){
+			throw ProtocolViolationException("Read Int");
+		}
+		return readBytes(conn);
+	}
+
+	/*
+	* Read an integer from a client.
+	*/
+	static string readString(const shared_ptr<Connection>& conn) {
+		if(readCode(conn) != Protocol::PAR_STRING){
+			throw ProtocolViolationException("Read String");
+		}
+		string s;
+		int n = readBytes(conn);
+
+		for(int i = 0; i < n; ++i){
+			s += readCode(conn);
+		}
+		return s;
+	}
+
+	/*
+	* Send a string to a client.
+	*/
+	static void writeString(const shared_ptr<Connection>& conn, const string& s) {
+		conn->write(Protocol::PAR_STRING);
+		size_t value = s.size();
+		writeBytes(conn, value);
+		for (char c : s) {
+			conn->write(c);
+		}
+
+	}
+
+	/*
+	* Send an code to the server as one byte.
+	*/
+	static void writeCode(const shared_ptr<Connection>& conn, unsigned char value) {
+		conn->write(value);
+	}
+
+	/*
+	* Send an integer to the server as four bytes.
+	*/
+	static void writeNumber(const shared_ptr<Connection>& conn, int value) {
+		conn->write(Protocol::PAR_NUM);
+		writeBytes(conn, value);
+	}
 
 private:
-  int readBytes(const shared_ptr<Connection>& conn) {
-    unsigned char byte1 = conn->read();
-    unsigned char byte2 = conn->read();
-    unsigned char byte3 = conn->read();
-    unsigned char byte4 = conn->read();
-    return (byte1 << 24) | (byte2 << 16) | (byte3 << 8) | byte4;
-  }
+	static int readBytes(const shared_ptr<Connection>& conn) {
+		unsigned char byte1 = conn->read();
+		unsigned char byte2 = conn->read();
+		unsigned char byte3 = conn->read();
+		unsigned char byte4 = conn->read();
+		return (byte1 << 24) | (byte2 << 16) | (byte3 << 8) | byte4;
+	}
+
+	static void writeBytes(const shared_ptr<Connection>& conn, int value) {
+		conn->write((value >> 24) & 0xFF);
+		conn->write((value >> 16) & 0xFF);
+		conn->write((value >> 8) & 0xFF);
+		conn->write(value & 0xFF);
+	}
 };
