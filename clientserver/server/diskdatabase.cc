7abcf9137c3d1133db093a57def3ec27e9c2a615
dat11fak <dat11fak@student.lth.se>
2015-04-09 15:59:35 +0200

    disk

--- clientserver/server/diskdatabase.cc
+++ clientserver/server/diskdatabase.cc
@@ -3,8 +3,25 @@
 #include <utility>
 #include "nonewsgroupexception.h"
 #include "noarticleexception.h"
+#include <iostream>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-DiskDatabase::DiskDatabase(): newsGroupCounter(0) {}
+using namespace std;
+
+DiskDatabase::DiskDatabase(): newsGroupCounter(0) {
+  cout << "Initializing diskdata (" << mkdir("diskdata", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) << ")" << endl;
+}
+
+bool DiskDatabase::addNewsgroup(std::string name){
+  if (find_if(newsGroups.begin(), newsGroups.end(),[&name](std::unordered_map<size_t,Newsgroup>::value_type& elem){return name == elem.second.getName();}) != newsGroups.end()){
+      return false;
+  }
+  ++newsGroupCounter;
+  Newsgroup group(name,newsGroupCounter);
+  newsGroups.insert(std::pair<size_t,Newsgroup>(newsGroupCounter,group));
+  return true;
+}
 
 std::vector<Newsgroup> DiskDatabase::getNewsgroups(){
   std::vector<Newsgroup> result;
@@ -14,6 +31,28 @@ std::vector<Newsgroup> DiskDatabase::getNewsgroups(){
   sort(result.begin(),result.end(),[](const Newsgroup& group1, const Newsgroup& group2){return group1.getID() < group2.getID();});
   return result;
 }
+
+bool DiskDatabase::removeNewsgroup(size_t nGroupID){
+  auto it = newsGroups.find(nGroupID);
+  if (it != newsGroups.end()){
+    newsGroups.erase(it);
+    articles[nGroupID].clear();
+    return true;
+  }else{
+    return false;
+  }
+}
+
+bool DiskDatabase::addArticle(size_t nGroupID, std::string title, std::string author, std::string text){
+   if (newsGroups.find(nGroupID) != newsGroups.end()){
+     ++articleCounters[nGroupID];
+     Article art(title, author, text, articleCounters[nGroupID]);
+     articles[nGroupID].insert(std::pair<size_t,Article>(articleCounters[nGroupID],art));
+     return true;
+   }else{
+     return false;
+   }
+ }
 
 std::vector<Article> DiskDatabase::getArticles(size_t nGroupID){
   std::vector<Article> result;
@@ -41,42 +80,9 @@ Article DiskDatabase::getArticle(size_t nGroupID, size_t articleID){
   }
 }
 
-bool DiskDatabase::addNewsgroup(std::string name){
-  if (find_if(newsGroups.begin(), newsGroups.end(),[&name](std::unordered_map<size_t,Newsgroup>::value_type& elem){return name == elem.second.getName();}) != newsGroups.end()){
-      return false;
-  }
-  ++newsGroupCounter;
-  Newsgroup group(name,newsGroupCounter);
-  //newsGroups[newsGroupCounter] = group;
-  newsGroups.insert(std::pair<size_t,Newsgroup>(newsGroupCounter,group));
-  return true;
-}
 
-bool DiskDatabase::removeNewsgroup(size_t nGroupID){
-  auto it = newsGroups.find(nGroupID);
-  if (it != newsGroups.end()){
-    newsGroups.erase(it);
-    articles[nGroupID].clear();
-    return true;
-  }else{
-    return false;
-  }
-}
 
-bool DiskDatabase::addArticle(size_t nGroupID, std::string title, std::string author, std::string text){
-   if (newsGroups.find(nGroupID) != newsGroups.end()){
-    //  if (find_if(articles[nGroupID].begin(), articles[nGroupID].end(),[&title](std::unordered_map<size_t,Article>::value_type& elem){return title == elem.second.getTitle();}) != articles[nGroupID].end()){
-    //      return false;
-    //  }
-     ++articleCounters[nGroupID];
-     Article art(title, author, text, articleCounters[nGroupID]);
-     //articles[articleCounters[nGroupID]] = art;
-     articles[nGroupID].insert(std::pair<size_t,Article>(articleCounters[nGroupID],art));
-     return true;
-   }else{
-     return false;
-   }
- }
+
 
 void DiskDatabase::removeArticle(size_t nGroupID, size_t articleID){
   if (newsGroups.find(nGroupID) != newsGroups.end()){
